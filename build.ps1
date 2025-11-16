74ae202ee722ba89a1738d5bd38d00106cf496b8
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2025-11-16 01:24:39 +0000

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,9 +1,9 @@
 ﻿Remove-Item –path ./client –recurse
 
-Invoke-WebRequest -Uri 'https://api.cloudmersive.com/cdr/docs/v1/swagger' -OutFile '.\cdr-api-swagger.json'
-(Get-Content .\cdr-api-swagger.json).replace('localhost', "api.cloudmersive.com") | Set-Content .\cdr-api-swagger.json
-(Get-Content .\cdr-api-swagger.json).replace('"http"', '"https"') | Set-Content .\cdr-api-swagger.json
+Invoke-WebRequest -Uri 'https://api.cloudmersive.com/fraud-ai/docs/v1/swagger' -OutFile '.\fraud-api-swagger.json'
+(Get-Content .\fraud-api-swagger.json).replace('localhost', "api.cloudmersive.com") | Set-Content .\fraud-api-swagger.json
+(Get-Content .\fraud-api-swagger.json -Raw) -replace '"http"','"https"' | Set-Content .\fraud-api-swagger.json -Encoding UTF8
 
-& java -jar swagger-codegen-cli.jar generate -i .\cdr-api-swagger.json -l objc -o client -c packageconfig.json
-(Get-Content ./client/CloudmersiveCDRApiClient.podspec).replace('CloudmersiveCDRApiClient/**/*.{m,h}', "client/CloudmersiveCDRApiClient/**/*.{m,h}") | Set-Content ./client/CloudmersiveCDRApiClient.podspec
-(Get-Content ./client/CloudmersiveCDRApiClient.podspec).replace('CloudmersiveCDRApiClient/**/*.h', "client/CloudmersiveCDRApiClient/**/*.h") | Set-Content ./client/CloudmersiveCDRApiClient.podspec
+& java -jar swagger-codegen-cli.jar generate -i .\fraud-api-swagger.json -l objc -o client -c packageconfig.json
+(Get-Content ./client/CloudmersiveFraudApiClient.podspec).replace('CloudmersiveFraudApiClient/**/*.{m,h}', "client/CloudmersiveFraudApiClient/**/*.{m,h}") | Set-Content ./client/CloudmersiveFraudApiClient.podspec
+(Get-Content ./client/CloudmersiveFraudApiClient.podspec).replace('CloudmersiveFraudApiClient/**/*.h', "client/CloudmersiveFraudApiClient/**/*.h") | Set-Content ./client/CloudmersiveFraudApiClient.podspec
